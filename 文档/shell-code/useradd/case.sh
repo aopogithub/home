67a7143d682aa81a4a2f39293dece41862b00254
root <root@room9pc01.tedu.cn>
2018-05-11 17:19:26 +0800

    xx

--- 文档/shell-code/useradd/case.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-case $1 in redhat)
-  echo "fedora" ;;
-           fedora)
-  echo "redhat" ;;
-                *) 
-  echo "/root/foo.sh redhat|fedora" >&2
-  exit 3
-esac
--- 文档/shell-code/useradd/useradd1.1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "请输入用户名：" user
-[ -z $user ]&&exit
-read -p "请输入密码：" pass
-useradd $user
-echo "${pass:-123456}" | passwd --stdin $user
