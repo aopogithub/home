5cae748c8577321af451bbfeaba7c7d194d71f94
“aopogithub” <fanaoao@hotmail.com>
2021-08-07 00:58:47 +0800

    修改kubeadm-images.sh

--- kubeadm-images.sh
+++ kubeadm-images.sh
@@ -9,7 +9,7 @@ kube_scheduler_version=$(awk -F/ '/kube-scheduler/{print $2}' $imagelistfile)
 kube_proxy_version=$(awk -F/ '/kube-proxy/{print $2}' $imagelistfile)
 pause_version=$(awk -F/ '/pause/{print $2}' $imagelistfile)
 etcd_version=$(awk -F/ '/etcd/{print $2}' $imagelistfile)
-coredns_version=$(awk -F/ '/coredns/{print $2}' $imagelistfile)
+coredns_version=$(awk -F/ '/coredns/{print $NF}' $imagelistfile)
 
 
 docker pull registry.aliyuncs.com/google_containers/$kube_apiserver_version
@@ -18,7 +18,7 @@ docker pull registry.aliyuncs.com/google_containers/$kube_scheduler_version
 docker pull registry.aliyuncs.com/google_containers/$kube_proxy_version
 docker pull registry.aliyuncs.com/google_containers/$pause_version
 docker pull registry.aliyuncs.com/google_containers/$etcd_version
-docker pull registry.aliyuncs.com/google_containers/$coredns_version
+docker pull registry.aliyuncs.com/google_containers/coredns
 
 
 docker tag registry.aliyuncs.com/google_containers/$kube_apiserver_version k8s.gcr.io/$kube_apiserver_version
@@ -27,7 +27,7 @@ docker tag registry.aliyuncs.com/google_containers/$kube_scheduler_version k8s.g
 docker tag registry.aliyuncs.com/google_containers/$kube_proxy_version k8s.gcr.io/$kube_proxy_version
 docker tag registry.aliyuncs.com/google_containers/$pause_version k8s.gcr.io/$pause_version
 docker tag registry.aliyuncs.com/google_containers/$etcd_version k8s.gcr.io/$etcd_version
-docker tag registry.aliyuncs.com/google_containers/$coredns_version k8s.gcr.io/$coredns_version
+docker tag registry.aliyuncs.com/google_containers/coredns:latest k8s.gcr.io/coredns/$coredns_version
 
 
 
@@ -37,4 +37,4 @@ docker image rm registry.aliyuncs.com/google_containers/$kube_scheduler_version
 docker image rm registry.aliyuncs.com/google_containers/$kube_proxy_version
 docker image rm registry.aliyuncs.com/google_containers/$pause_version
 docker image rm registry.aliyuncs.com/google_containers/$etcd_version
-docker image rm registry.aliyuncs.com/google_containers/$coredns_version
+docker image rm registry.aliyuncs.com/google_containers/coredns:latest
