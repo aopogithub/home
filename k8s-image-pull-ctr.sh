17d1e26ec732ee958b8ebf9171923d24342fec51
aopogithub <fanaoao@hotmail.com>
2022-05-19 23:40:23 +0800

    add crictl.yaml

--- k8s-image-pull-ctr.sh
+++ k8s-image-pull-ctr.sh
@@ -12,29 +12,29 @@ etcd_version=$(awk -F/ '/etcd/{print $2}' $imagelistfile)
 coredns_version=$(awk -F/ '/coredns/{print $NF}' $imagelistfile)
 
 
-ctr images pull registry.aliyuncs.com/google_containers/$kube_apiserver_version
-ctr images pull registry.aliyuncs.com/google_containers/$kube_controller_manager_version
-ctr images pull registry.aliyuncs.com/google_containers/$kube_scheduler_version
-ctr images pull registry.aliyuncs.com/google_containers/$kube_proxy_version
-ctr images pull registry.aliyuncs.com/google_containers/$pause_version
-ctr images pull registry.aliyuncs.com/google_containers/$etcd_version
-ctr images pull registry.aliyuncs.com/google_containers/coredns:latest
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$kube_apiserver_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$kube_controller_manager_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$kube_scheduler_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$kube_proxy_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$pause_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/$etcd_version
+ctr --namespace=k8s.io images pull registry.aliyuncs.com/google_containers/coredns:latest
 
 
-ctr images tag registry.aliyuncs.com/google_containers/$kube_apiserver_version k8s.gcr.io/$kube_apiserver_version
-ctr images tag registry.aliyuncs.com/google_containers/$kube_controller_manager_version k8s.gcr.io/$kube_controller_manager_version
-ctr images tag registry.aliyuncs.com/google_containers/$kube_scheduler_version k8s.gcr.io/$kube_scheduler_version
-ctr images tag registry.aliyuncs.com/google_containers/$kube_proxy_version k8s.gcr.io/$kube_proxy_version
-ctr images tag registry.aliyuncs.com/google_containers/$pause_version k8s.gcr.io/$pause_version
-ctr images tag registry.aliyuncs.com/google_containers/$etcd_version k8s.gcr.io/$etcd_version
-ctr images tag registry.aliyuncs.com/google_containers/coredns:latest k8s.gcr.io/coredns/$coredns_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$kube_apiserver_version k8s.gcr.io/$kube_apiserver_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$kube_controller_manager_version k8s.gcr.io/$kube_controller_manager_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$kube_scheduler_version k8s.gcr.io/$kube_scheduler_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$kube_proxy_version k8s.gcr.io/$kube_proxy_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$pause_version k8s.gcr.io/$pause_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/$etcd_version k8s.gcr.io/$etcd_version
+ctr --namespace=k8s.io images tag registry.aliyuncs.com/google_containers/coredns:latest k8s.gcr.io/coredns/$coredns_version
 
 
 
-ctr images rm registry.aliyuncs.com/google_containers/$kube_apiserver_version
-ctr images rm registry.aliyuncs.com/google_containers/$kube_controller_manager_version
-ctr images rm registry.aliyuncs.com/google_containers/$kube_scheduler_version
-ctr images rm registry.aliyuncs.com/google_containers/$kube_proxy_version
-ctr images rm registry.aliyuncs.com/google_containers/$pause_version
-ctr images rm registry.aliyuncs.com/google_containers/$etcd_version
-ctr images rm registry.aliyuncs.com/google_containers/coredns:latest
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$kube_apiserver_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$kube_controller_manager_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$kube_scheduler_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$kube_proxy_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$pause_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/$etcd_version
+ctr --namespace=k8s.io images rm registry.aliyuncs.com/google_containers/coredns:latest
